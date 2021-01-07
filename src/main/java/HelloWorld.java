0dbbea7a19279c0465550db6ff8e167ede8dd405
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-01-07 13:10:16 +0100

    gitignore

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Monika");
+        System.out.println(28);
+        System.out.println('M');
+    }
+}
