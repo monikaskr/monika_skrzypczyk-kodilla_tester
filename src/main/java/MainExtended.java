4a0631ae13c29b3c9b6f6213ed5ee6bed36c145e
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-01-08 13:46:55 +0100

    Initial version

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Monika";
+        int myAge = 28;
+        double height = 2.22;
+        char carClass = 'M';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println("Jestem super");
+    }
+}
