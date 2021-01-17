244cbbb02897ac77f8b6bc1a8b33a2ba96b40375
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-01-17 18:46:32 +0100

    dodającą przekazaną w argumencie ocenę do tablicy, zwracającą ostatnio dodaną ocenę, zwracającą średnią ocen.

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,32 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int end() {
+        if(size == 0){
+            return 0;
+        }
+        return this.grades[this.size-1];
+    }
+    public int average() {
+        int sum = 0 ;
+        for ( int i = 0; i < this.size; i ++ ) {
+            sum = sum + this.grades[i];
+        }
+        return sum / this.size;
+    }
+}
+
