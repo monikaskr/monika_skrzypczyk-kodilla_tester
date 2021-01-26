d18c4c12b1a9e05f31d8c5851e27b5fd1d651ad4
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-01-26 21:06:35 +0100

    testy jednostkowe dla kalkulatora

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+    @Test
+    public void testMinus() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int minResult = calculator.sum(a, b);
+        assertEquals(13, minResult);
+    }
+    @Test
+        public void testKwadrat() {
+        Calculator calculator = new Calculator();
+        int a = -4;
+        int b = 8;
+        int kwadratResult = calculator.kwadrat(a, b);
+        assertEquals(13, kwadratResult);
+    }
+}
