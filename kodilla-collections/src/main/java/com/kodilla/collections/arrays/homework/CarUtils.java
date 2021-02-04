b724b324d3c8ca2a2e1bbfec1ca17b5a987dd8ab
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-02-04 13:43:34 +0100

    losowanie wartości i iterowanie po tablicy

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -0,0 +1,28 @@
+package com.kodilla.collections.arrays.homework;
+
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Mazda;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+public class CarUtils {
+
+    public static void displayCarInfo(Car car) {
+        System.out.println("-----------------------------");
+        System.out.println("Car kind: " + describeCar(car));
+        System.out.println("Car increaseSpeed: "); car.increaseSpeed();
+        System.out.println("Car decreaseSpeed: "); car.decreaseSpeed();
+    }
+
+    public static String describeCar(Car car){
+        if (car instanceof Ford)
+            return "Ford";
+        else if (car instanceof Mazda)
+            return "Mazda";
+        else if (car instanceof Opel)
+            return "Opel";
+        else
+            return "Unknown car name";
+    }
+
+}
