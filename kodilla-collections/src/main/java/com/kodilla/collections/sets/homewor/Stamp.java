38330967450edf80d7348e9375156dd82aa3a589
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-02-05 16:40:33 +0100

    zbiory - kolekcja znaczków, usuwanie powielanych metod

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homewor/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homewor/Stamp.java
@@ -5,38 +5,45 @@ import java.util.Objects;
 public class Stamp {
     private String name;
     private double dimension;
-    private String marked;
+    private boolean stamp;
 
-    public Stamp (String  name, double dimension, String marked){
+
+    public Stamp (String  name, double dimension, boolean stamp){
         this.name = name;
         this.dimension = dimension;
-        this.marked = marked;
+        this.stamp = stamp;
     }
-    public String getrName(){
+
+    public String getName(){
         return name;
     }
     public double getDimension(){
         return dimension;
     }
-    public String getMarked(){
-        return marked;
+
+    public boolean isStamp(){
+        return stamp;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Stamp stamp = (Stamp) o;
-        return Double.compare(stamp.dimension, dimension) == 0 && Objects.equals(name, stamp.name) && Objects.equals(marked, stamp.marked);
+        Stamp stamp1 = (Stamp) o;
+        return Double.compare(stamp1.dimension, dimension) == 0 && stamp == stamp1.stamp && Objects.equals(name, stamp1.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, dimension, marked);
+        return Objects.hash(name, dimension, stamp);
     }
+
     @Override
     public String toString() {
-        return "Stamp{" + "stamp Name'" + name + '\'' + ", dimension='"
-                + dimension + '\'' + ", marked or not=" + marked + '}';
+        return "Stamp{" +
+                "name='" + name + '\'' +
+                ", dimension=" + dimension +
+                ", stamp=" + stamp +
+                '}';
     }
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homewor/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homewor/StampsApplication.java
@@ -1,19 +1,18 @@
 package com.kodilla.collections.sets.homewor;
 
-import com.kodilla.collections.sets.Order;
-
 import java.util.HashSet;
 import java.util.Set;
 
 public class StampsApplication {
     public static void main(String[] args) {
+
         Set<Stamp> stamps = new HashSet<>();
-        stamps.add(new Stamp("a", 2, "Y"));
-        stamps.add(new Stamp("b", 2.5, "Y"));
-        stamps.add(new Stamp("c", 1, "N"));
-        stamps.add(new Stamp("d", 1.5, "N"));
-        stamps.add(new Stamp("b", 2.5, "Y"));
-        stamps.add(new Stamp("d", 1.5, "N"));
+        stamps.add(new Stamp("a", 2, true));
+        stamps.add(new Stamp("b", 2.5, false));
+        stamps.add(new Stamp("c", 1, true));
+        stamps.add(new Stamp("d", 1.5, false));
+        stamps.add(new Stamp("b", 2.5, true));
+        stamps.add(new Stamp("d", 1.5, false));
 
         System.out.println(stamps.size());
         for (Stamp stamp : stamps)
