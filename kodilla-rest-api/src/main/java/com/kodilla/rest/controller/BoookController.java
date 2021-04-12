819621d2c87a0f24238fa8649199438a2790214f
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-04-12 19:09:20 +0200

    API

--- kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BoookController.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BoookController.java
@@ -2,14 +2,14 @@ package com.kodilla.rest.controller;
 
 import com.kodilla.rest.domain.BookDto;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMpping;
+import org.springframework.web.bind.annotation.RequestMpping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-//@RequestMpping("/books")
+@RequestMpping("/books")
 public class BoookController {
 
     @GetMapping
