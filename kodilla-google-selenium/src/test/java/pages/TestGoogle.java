bb22c27f66a61052e12ccbef8d4433fbc0aaa8b6
Monika Skrzypczyk <skrzypczyk92@gmail.com>
2021-04-17 13:10:58 +0200

    Page Object Model

---
+++ kodilla-google-selenium/src/test/java/pages/TestGoogle.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestGoogle {
+    WebDriver driver;
+
+    @Before
+    public void testSetup() {
+        System.setProperty("webdriver.chrome.driver", "H:\\Kodilla\\kodilla-google-selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://wwww.google.com");
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void testGooglePage() {
+        GoogleSearch googleSearch = new GoogleSearch(driver);
+        googleSearch.searchResults();
+    }
+}
